9e79398e5351f15a3e1df355a97281309c046a7b
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-07-05 13:12:26 -0500

    started working on project page

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -2,12 +2,16 @@ import {ReactNode} from "react";
 import {useLocation} from "react-router-dom";
 
 type propsContent = { children: ReactNode}
+
+
+
+
 export default function Content({children}:propsContent){
 
     const location = useLocation()
 
     return(
-        <div className={`bg-[#F1F1F1] ${location.pathname !== "/coreview/c" ? ("p-5"):("")} flex w-full overflow-y-scroll`}>
+        <div className={`bg-[#F1F1F1] ${ ((location.pathname !== "/coreview/c") && (location.pathname !== "/coreview/projects/open")) ? ("p-5"):("")} flex w-full overflow-y-scroll`}>
              {children}
         </div>
     )
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,20 +1,46 @@
-import {OptionsbarButtonStyle} from "./clients.tsx";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import { IoIosArrowBack } from "react-icons/io";
 
+
+type propsOptionBarStyle = { action: ()=>void, optionalStyle?:string}
+export function OptionsbarButtonStyle( {action, optionalStyle}:propsOptionBarStyle){
+
+
+    const style = useState({backgroundColor:""})
+
+    const timout = () =>{
+
+        setTimeout(() => {
+            style[1]({backgroundColor:""})
+            setTimeout(() => {
+                action()
+            }, 50); // 1000 milliseconds equals 1 second
+        }, 60); // 1000 milliseconds equals 1 second
+    }
+
+    return(
+        <button onMouseLeave={()=> style[1]({backgroundColor:""})} onMouseDown={()=> style[1]({backgroundColor:"#252525"})} onMouseUp={()=>timout()}
+                className={` p-[2px] px-4   text-sm  h-full ${optionalStyle}`} style={style[0]}>
+            <IoIosArrowBack />
+        </button>
+
+    )
+}
 
 export default function Project(){
 
     const navigate = useNavigate()
 
     return(
-        <div className={"flex flex-col w-full"}>
-            <div className={"flex w-full"}>
-                <div>
-                    <OptionsbarButtonStyle action={()=>navigate("/coreview/projects")} title={"back"}/>
+        <div className={"flex flex-col w-full pt-12 "}>
+            <div className={"flex w-full h-12 bg-[#171717] fixed top-0 text-white flex flex-row"}>
+                <OptionsbarButtonStyle action={()=>navigate("/coreview/projects")}/>
+                <div className={" flex flex-row grid content-center"}>
+                    <p className={" font-medium ml-2"} >Projects </p>
                 </div>
-                <p className={"text-black font-medium ml-2"} >Projects </p>
             </div>
-            <div className={"pb-20 grid grid-cols-2 gap-3 mt-5"}>
+            <div className={"pb-20 grid grid-cols-2 gap-3  p-5"}>
                 <div className={ "border border-[#E5E5E5]  text-[#616161] text-sm    rounded-lg "}>
                     <p className={"py-1 pl-2"}>Media</p>
                     <div className={"bg-white h-64 rounded-b-lg"}>
@@ -49,6 +75,7 @@ export default function Project(){
                         <p className={"text-black p-1 "}> #content# </p>
                     </div>
                 </div>
+
             </div>
 
         </div>
