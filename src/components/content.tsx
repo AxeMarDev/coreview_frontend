3418be32e08184fb73d74a94d17baf81d13e72ca
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-07-08 01:53:25 -0500

    fixed navbar appear boolean

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -12,7 +12,7 @@ export default function Content({children}:propsContent){
 
     return(
         <div className={`bg-[#F1F1F1] ${ (
-            (location.pathname.substring(0,11) === "/coreview/c" && ( (location.pathname.length === 11) || (location.pathname[12] === "/") ))  || 
+            (location.pathname.substring(0,11) === "/coreview/c" && ( (location.pathname.length === 11) || (location.pathname[11] === "/") ))  || 
             (location.pathname.substring(0,23) === "/coreview/projects/open")) 
             ? (""):("p-5")} flex w-full overflow-y-scroll`}>
 
